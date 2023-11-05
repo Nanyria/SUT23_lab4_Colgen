f866a1d64827b5f5b6169a18b125cc3ae7995aa7
Nathalee Wilund <nathalee.wilund@edu.varberg.se>
2023-11-05 13:09:50 +0100

    Skapat klassen Employee

---
+++ Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT23_lab4_Colgen
+{
+    internal class Employee
+    {
+        public int _id { get; set; }
+        public string _name { get; set; }
+        public string _gender { get; set; }
+        public double _salary { get; set; }
+
+        public Employee(int id, string name, string gender, double salary)
+        {
+            _id = id;
+            _name = name;
+            _gender = gender;
+            _salary = salary;
+        }
+    }
+}
